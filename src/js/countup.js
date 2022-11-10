2c93e756c11c1231d2c83cc0c296d3e4e0c38f4c
Andriy Panasiuk <panasiukmail@gmail.com>
2022-11-10 15:35:11 +0200

    fix:  fixed counter

--- src/js/countup.js
+++ src/js/countup.js
@@ -1,29 +1,31 @@
-window.onload = function () {
-    countUpFromTime("Feb 24,2022,03:20:00", 'countup1');
-};
+export function counter() {
+    window.onload = function () {
+        countUpFromTime("Feb 24,2022,03:20:00", 'countup1');
+    }
 
-export function countUpFromTime(countFrom, id) {
-    countFrom = new Date(countFrom).getTime();
+    function countUpFromTime(countFrom, id) {
+        countFrom = new Date(countFrom).getTime();
 
-    var now = new Date(),
-        countFrom = new Date(countFrom),
-        timeDifference = (now - countFrom);
+        var now = new Date(),
+            countFrom = new Date(countFrom),
+            timeDifference = (now - countFrom);
 
-    var secondsInADay = 60 * 60 * 1000 * 24,
-        secondsInAHour = 60 * 60 * 1000;
+        var secondsInADay = 60 * 60 * 1000 * 24,
+            secondsInAHour = 60 * 60 * 1000;
 
-    days = Math.floor(timeDifference / (secondsInADay) * 1);
-    hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-    mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-    secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+        var days = Math.floor(timeDifference / (secondsInADay) * 1),
+            hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1),
+            mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1),
+            secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
 
-    var idElement = document.getElementById(id);
+        var idElement = document.getElementById(id);
 
-    idElement.getElementsByClassName('days')[0].innerHTML = days;
-    idElement.getElementsByClassName('hours')[0].innerHTML = hours;
-    idElement.getElementsByClassName('minutes')[0].innerHTML = mins;
-    idElement.getElementsByClassName('seconds')[0].innerHTML = secs;
+        idElement.getElementsByClassName('days')[0].innerHTML = days;
+        idElement.getElementsByClassName('hours')[0].innerHTML = hours;
+        idElement.getElementsByClassName('minutes')[0].innerHTML = mins;
+        idElement.getElementsByClassName('seconds')[0].innerHTML = secs;
 
-    clearTimeout(countUpFromTime.interval);
-    countUpFromTime.interval = setTimeout(function () { countUpFromTime(countFrom, id); }, 1000);
+        clearTimeout(countUpFromTime.interval);
+        countUpFromTime.interval = setTimeout(function () { countUpFromTime(countFrom, id); }, 1000);
+    }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import '../style/main.scss'
 import '../style/countup.scss'
 import '../style/fog.scss'
 import '../style/background.scss'
-import { countUpFromTime } from './countup'
+import { counter } from './countup.js'
 
-countUpFromTime();
+counter();
